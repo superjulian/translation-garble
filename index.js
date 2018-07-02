1e3ae1f6b58455678c94b7e1cb45c59082f12c2b
Julian Meltzer <julianpmeltzer@gmail.com>
2018-07-02 15:42:35 -0400

    Now displays list of languages used. Also changed logging behaviour on input longer than 30 chars.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ var langs = Object.keys(languages);
 function randLang (){
     return langs [Math.floor(Math.random() * (langs.length - 2))];
 }
-function garble (text, times = 30, recipient){
+function garble (text, times = 3, recipient, langs_used){
     //console.log (text);
     //console.log (times);
     if (times > 0){
-        translate(text , {to: randLang()}
+        lang_key = randLang();
+        translate(text , {to: lang_key}
         ).then ( res => {
-           garble (res.text, times - 1, recipient);
+           garble (res.text, times - 1, recipient, langs_used + " → " + languages[lang_key]);
         }).catch (err => {
             console.error(err);
         });
@@ -22,8 +23,14 @@ function garble (text, times = 30, recipient){
     else {
         translate(text, {to: 'en'}
         ).then (res => {
-            console.log ("Finished garble\": " + res.text +"\"");
-            recipient.send (res.text);
+            langs_used += " →` English";
+            if (res.text.length < 30) {
+                console.log ("Finished garble: \"" + res.text +"\"");
+            }
+            else {
+                console.log ("Finished garble: \"" + res.text.slice(0, 27) +"...\"");
+            }
+            recipient.send (res.text + "<p2 style =\"font-size:.8em; color:#36454f;\" ><br><hr>" + langs_used + "</p2>");
         }).catch(err => {
             console.error(err);
         });
@@ -33,8 +40,13 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 app.post('/', function (req, res) {
-        //console.log (req.body);
+    //console.log (req.body);
+    if (req.body.text.length < 30) {
         console.log("Recived request: \"" + req.body.text + "\" for " + req.body.count + " rounds.");
-        garble (req.body.text, req.body.count , res);
+    }
+    else {
+        console.log("Recived request: \"" + req.body.text.slice(0,27) + "...\" for " + req.body.count + " rounds.");
+    }
+    garble (req.body.text, req.body.count , res, "Your input");
 })
 app.listen(process.env.PORT || 5000)
