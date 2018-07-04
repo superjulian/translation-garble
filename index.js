9ca6d56d47d1ae5c44e70604f3575569c4086293
Julian Meltzer <julianpmeltzer@gmail.com>
2018-07-04 14:25:30 -0400

    Added a /plainText handeler which returns translated text w/o list of langs

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ var langs = Object.keys(languages);
 function randLang (){
     return langs [Math.floor(Math.random() * (langs.length - 2))];
 }
-function garble (text, times = 3, recipient, langs_used){
+function garble (text, times = 3, recipient, langs_used, plainText = true){
     //console.log (text);
     //console.log (times);
     if (times > 0){
         lang_key = randLang();
         translate(text , {to: lang_key}
         ).then ( res => {
-           garble (res.text, times - 1, recipient, langs_used + " → " + languages[lang_key]);
+           garble (res.text, times - 1, recipient, langs_used + " → " + languages[lang_key], plainText);
         }).catch (err => {
             console.error(err);
         });
@@ -30,7 +30,12 @@ function garble (text, times = 3, recipient, langs_used){
             else {
                 console.log ("Finished garble: \"" + res.text.slice(0, 27) +"...\"");
             }
-            recipient.send (res.text + "<p2 style =\"font-size:.8em; color:#36454f;\" ><br><hr>" + langs_used + "</p2>");
+            if (!plainText){
+                recipient.send (res.text + "<p2 style =\"font-size:.8em; color:#36454f;\" ><br><hr>" + langs_used + "</p2>");
+            }
+            else {
+                recipient.send (res.text);
+            }
         }).catch(err => {
             console.error(err);
         });
@@ -47,6 +52,16 @@ app.post('/', function (req, res) {
     else {
         console.log("Recived request: \"" + req.body.text.slice(0,27) + "...\" for " + req.body.count + " rounds.");
     }
-    garble (req.body.text, req.body.count , res, "Your input");
+    garble (req.body.text, req.body.count , res, "Your input", false);
+})
+app.post('/plainText', function (req, res) {
+    //console.log (req.body);
+    if (req.body.text.length < 30) {
+        console.log("Recived plain text request: \"" + req.body.text + "\" for " + req.body.count + " rounds.");
+    }
+    else {
+        console.log("Recived plain text request: \"" + req.body.text.slice(0,27) + "...\" for " + req.body.count + " rounds.");
+    }
+    garble (req.body.text, req.body.count , res, "Your input", true);
 })
 app.listen(process.env.PORT || 5000)
