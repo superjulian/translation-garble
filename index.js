66cc56469dd44e090248af576b6f6d666984a352
Julian Meltzer <julianpmeltzer@gmail.com>
2018-07-04 17:21:22 -0400

    added COR support maybe?

--- index.js
+++ index.js
@@ -40,7 +40,13 @@ function garble (text, times = 3, recipient, langs_used, plainText = true){
             console.error(err);
         });
     }
-}   
+}
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
